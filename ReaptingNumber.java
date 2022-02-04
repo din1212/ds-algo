00dc256992f0528e5356755a86eebea8eadfafd5
Dinesh Musali <68723922+din1212@users.noreply.github.com>
2022-02-04 10:06:39 +0530

    Create ReaptingNumber.java

---
+++ ReaptingNumber.java
@@ -0,0 +1,8 @@
+// Given an integer array A of size N, find the first repeating element in it. We need to find the element that occurs more than once and whose index of first occurrence is smallest.
+// If there is no repeating element, return -1.
+
+// Input 1:  A = [10, 5, 3, 4, 3, 5, 6]
+// Input 2:  A = [6, 10, 5, 4, 9, 120]
+
+// Output 1: 5
+// Output 2: -1
